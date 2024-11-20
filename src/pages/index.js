728b094da68a0787426c5ce9bc7d59ca3c819d57
Mohamed Ali <Mohamed.Ali@bravebison.com>
2024-11-20 15:49:24 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -168,6 +168,6 @@ const IndexPage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Coming soon" />
 
 export default IndexPage
